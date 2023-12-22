17b1ad391be04f46b15d53850da654057455016d
Phung Hoang <phunghoangvnuit@gmail.com>
2023-12-22 13:39:39 +0700

    v0.0.8

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 console.log('config.js');
-console.log('config.js is v0.0.7');
+console.log('config.js is v0.0.8');
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log('index.js');
-console.log('index.js is v0.0.7');
+console.log('index.js is v0.0.8');
